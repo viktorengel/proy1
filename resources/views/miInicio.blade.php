4aa54740080998c88f17d6ca30c549a162c95cb3
Victor Rengel <viktorengel@hotmail.com>
2023-05-08 10:44:36 -0500

    Motor plantillas blade

--- resources/views/miInicio.blade.php
+++ resources/views/miInicio.blade.php
@@ -1,3 +1,29 @@
+@extends('layouts.base')
+
+@section('principal')
 <h1>Home Page Laravel</h1>
 
-<p>Viktor Rengel</p>
+<div id="carouselExample" class="carousel slide">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="https://img.freepik.com/vector-premium/banner-diseno-colorido-azul-oscuro-digital-tecnologia-red_181182-33509.jpg" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="https://img.freepik.com/vector-gratis/fondo-banner-tecnologia-formas-hexagonales-espacio-texto_1017-22589.jpg" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="https://thumbs.dreamstime.com/b/tecnolog%C3%ADa-engranajes-ordenador-banner-fondo-fuente-circuito-futurista-c%C3%ADrculo-azul-rel%C3%A1mpago-electricidad-abstracto-vector-220508888.jpg" class="d-block w-100" alt="...">
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+
+
+@endsection
--- resources/views/misproductos.blade.php
+++ resources/views/misproductos.blade.php
@@ -1,4 +1,11 @@
+@extends('layouts.base')
+
+@section('principal')
+
+
 <h1>Mis Productos</h1>
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat aperiam cum laborum ex enim eum eaque soluta ab quisquam neque ad voluptas sit, illo optio voluptatibus assumenda vitae dolor molestias!
-Animi cupiditate quam iste aut provident itaque impedit neque laborum accusamus ad aliquid pariatur est officiis ea repudiandae quia aperiam voluptatem, officia delectus nisi corrupti eligendi mollitia doloribus explicabo! Consequatur?
-Doloribus aspernatur alias nam, dicta sequi, eius dolores reprehenderit ipsa inventore voluptatum iure? Accusamus, totam eum molestias nobis beatae odio, libero accusantium consequatur et, vel fugit! Repudiandae debitis fuga rem.</p>
+    Animi cupiditate quam iste aut provident itaque impedit neque laborum accusamus ad aliquid pariatur est officiis ea repudiandae quia aperiam voluptatem, officia delectus nisi corrupti eligendi mollitia doloribus explicabo! Consequatur?
+    Doloribus aspernatur alias nam, dicta sequi, eius dolores reprehenderit ipsa inventore voluptatum iure? Accusamus, totam eum molestias nobis beatae odio, libero accusantium consequatur et, vel fugit! Repudiandae debitis fuga rem.</p>
+
+@endsection
